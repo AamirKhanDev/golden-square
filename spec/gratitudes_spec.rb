a215f632b056f32a22a41712beb36cf8dec4c74c
HoodieB <aamirkhan94work@gmail.com>
2022-09-13 15:34:36 +0100

    gratitudes_spec.rb

--- spec/gratitudes_spec.rb
+++ spec/gratitudes_spec.rb
@@ -1,10 +1,23 @@
 require 'gratitudes'
 
 RSpec.describe Gratitudes do
-    it 'adds a new gratitude to the array'
-    gratitude = Gratitude.new
-    gratitude.add("Thankful for my friends")
-    result = gratitude.format()
-    expect(result).to eq "Be grateful for:Thankful for my friends"
+    it 'generates an empty array' do
+    gratitudes = Gratitudes.new
+    result = gratitudes.format()
+    expect(result).to eq "Be grateful for: "
 end
+
+    it 'adds in a single user input' do
+    gratitudes = Gratitudes.new
+    gratitudes.add("Oxygen")
+    expect(gratitudes.format).to eq "Be grateful for: Oxygen"
+end
+
+    it 'adds in multiple user inputs' do
+    gratitudes = Gratitudes.new
+    gratitudes.add("Oxygen")
+    gratitudes.add("Pizza")
+    gratitudes.add("Technology")
+    expect(gratitudes.format).to eq "Be grateful for: Oxygen, Pizza, Technology"
+ end
 end
