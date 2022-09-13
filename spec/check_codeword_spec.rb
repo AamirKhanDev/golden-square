949192ea0cb367f1dbb74a69e7afe43a91da17c3
HoodieB <aamirkhan94work@gmail.com>
2022-09-13 13:27:51 +0100

    check_codeword_spec.rb

--- spec/check_codeword_spec.rb
+++ spec/check_codeword_spec.rb
@@ -1,8 +1,18 @@
 require 'check_codeword.rb'
 
 RSpec.describe "check_codeword" do
-    it "checks if the codeword is input" do
+    it "returns a correct message if the codeword is input" do
         result = check_codeword("horse")
         expect(result).to eq ("Correct! Come in.")
     end
+
+    it "returns a wrong message if the codeword is input" do
+        result = check_codeword("green")
+        expect(result).to eq ("WRONG!")
+    end
+    
+     it "returns a close message if the codeword is input" do
+         result = check_codeword("house")
+        expect(result).to eq ("Close, but nope.")  
+end
 end
