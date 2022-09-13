215d863b52562662b2e95e6b81bd3ee579a74842
HoodieB <aamirkhan94work@gmail.com>
2022-09-13 15:14:23 +0100

    present_spec.rb

--- spec/present_spec.rb
+++ spec/present_spec.rb
@@ -1,10 +1,20 @@
 require 'present.rb'
 
 RSpec.describe Present do
-    context "contents are already wrapped" do
+    it "contents are ready to be wrapped and unwrapped" do
         present = Present.new
         present.wrap("Toy")
         expect(present.unwrap).to eq "Toy"
     end
 
+    it "fails if we unwrap without wrapping" do
+        present = Present.new
+        expect { present.unwrap }.to raise_error "No contents have been wrapped."
+    end
+
+        it "fails if we wrap if presents are already wrapped" do
+        present = Present.new
+        present.wrap("Toy")
+        expect { present.wrap("Game") }.to raise_error "A contents has already been wrapped."
+    end
 end
